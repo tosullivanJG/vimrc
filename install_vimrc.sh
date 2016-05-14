7b92d352b356bef34ca3b99c2fc2a1461ac22eb4
Pete Thompson <me@petethompson.com>
2016-05-14 23:46:00 +0100

    Add syntastic

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -5,6 +5,7 @@ echo 'set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/lib/vim-pathogen/autoload/pathogen.vim
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/pathogen.vim
-source ~/.vim_runtime/vimrcs/nerdtree.vim' > ~/.vimrc
+source ~/.vim_runtime/vimrcs/nerdtree.vim
+source ~/.vim_runtime/vimrcs/syntastic.vim' > ~/.vimrc
 
 echo "Vim configured successfully."
